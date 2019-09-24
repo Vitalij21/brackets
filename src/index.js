8dac7a4e9c805fc7e880d707b56a8457b5369820
Vitalij21 <41307680+Vitalij21@users.noreply.github.com>
2019-09-24 19:07:46 +0300

    20 passing of the task brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 !=0) {
+    return false;
+  }
+   else {
+     while (str.indexOf('()', 0) >=0 || str.indexOf('[]', 0) >=0 || str.indexOf('{}', 0) >=0 || str.indexOf('||', 0) >=0
+     || str.indexOf('12', 0) >=0 || str.indexOf('34', 0) >=0 || str.indexOf('56', 0) >=0 
+     || str.indexOf('77', 0) >=0 || str.indexOf('88', 0) >=0 ) {
+      str = str.replace('()', '').replace('[]', '').replace('{}', '').replace('||', '').replace('12', '').replace('34', '').
+      replace('56', '').replace('77', '').replace('88', '');
+      //console.log(str);
+     }
+     if (str.length == 0) {
+      return true;
+     } else {
+      return false;
+     }
+   }
 }
